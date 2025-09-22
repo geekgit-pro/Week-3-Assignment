4d287d8361ef181d4fd7ad2ec94f62321861ff9d
Aijaz <geeksen7@gmail.com>
2025-09-22 09:55:08 +0530

    Implemented POST /users/courses/:courseId

--- course_app/db/userModel.js
+++ course_app/db/userModel.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { boolean } = require('zod');
 
 
 mongoose.connect('mongodb+srv://geeksen7_admin:geeksen7admin@cluster0.hsykzlb.mongodb.net/app_course');
 
 const userSchema = new mongoose.Schema({
     username : String,
-    password : String
+    password : String,
+    purchasedCourse :[{
+        courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' }
+    }]
+   
 });
 
 const userModel = mongoose.model('Users', userSchema);
--- course_app/routes/userRoute.js
+++ course_app/routes/userRoute.js
@@ -4,6 +4,7 @@ const Course = require('../db/courseModel');
 const headerValidation = require('../middleware/headerMiddleware');
 const userValidation = require('../middleware/userMiddleware');
 const errorObj = require('../util/errorBuilder');
+const { route } = require('./adminRoute');
 const router = express.Router();
 
 
@@ -36,6 +37,58 @@ router.get('/courses', headerValidation, async function (req, res, next) {
         return next(errorObj.errorBuilder('INTERNAL SERVER ERROR'),500);
     }
 });
+
+router.post('/courses/:courseId', headerValidation, async function (req, res, next) {
+    const username = req.headers['username'];
+    const courseId = req.params.courseId;
+    
+    try {
+        const course = await Course.findById(courseId);
+        if(!course) {
+            return next(errorObj.errorBuilder('Course not found',401));
+        }
+
+       const updatedUser = await User.findOneAndUpdate(
+                                            { username },
+                                            { "$push" : { purchasedCourse: course._id } },
+                                            { new: true }
+                                    );
+        console.log(updatedUser);
+
+        if (!updatedUser) {
+            return next(errorObj.errorBuilder('User not found', 401));
+        }
+
+        return res.status(200).json({
+        message : `${username} purchased ${course}`
+    });
+ 
+    } catch (error) {
+        return next(errorObj.errorBuilder('INTERNAL SERVER ERROR',500));
+    }
+});
+
+// router.get('/purchasedCourse', headerValidation, async function (req, res, next) {
+//     const username = req.headers['username'];
+
+//     try {
+//         const user = await User.findOne({ username }).populate('purchasedCourses.courseId');
+
+//         if (!user) {
+//             return next(errorObj.errorBuilder('User not found', 404));
+//         }
+//         const purchasedCourses = user.purchasedCourse.map(pc => pc.courseId);
+
+//         return res.status(200).json({
+//             username: user.username,
+//             purchasedCourses
+//         });
+
+//     } catch (error) {
+//         console.error(error);
+//         return next(errorObj.errorBuilder('INTERNAL SERVER ERROR', 500));
+//     }
+// })
 
 
 module.exports = router;
